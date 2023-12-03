62e5205f0254f4150bff90c05ee15e8e0418921d
Prathyusha <145478140+ppapola@users.noreply.github.com>
2023-12-03 16:49:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
+// index.js
 
-async function runExample() {
-  var x = new Float32Array(1,11)
-   var x = [];
+async function generateSample() {
+    // Generate a random latent vector (adjust as needed)
+    const latentVector = Array.from({ length: 100 }, () => Math.random());
 
-     x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-     x[5] = document.getElementById('box6').value;
-     x[6] = document.getElementById('box7').value;
-     x[7] = document.getElementById('box8').value;
-     x[8] = document.getElementById('box9').value;
-     x[9] = document.getElementById('box10').value;
-     x[10] = document.getElementById('box11').value;
+    try {
+        // Send a POST request to the Flask backend
+        const response = await fetch('/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ latent_vector: latentVector }),
+        });
 
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 11]);
+        // Parse the response JSON
+        const result = await response.json();
 
-    let session = new onnx.InferenceSession();
-
-    await session.loadModel("./DLnet_WineData.onnx");
-    let outputMap = await session.run([tensorX]);
-    let outputData = outputMap.get('output1');
-
-   let predictions = document.getElementById('predictions');
-
-  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-   <table>
-     <tr>
-       <td>  Rating of Wine Quality  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td>
-     </tr>
-  </table>`;
-    
-
-
-}
-
+        // Display or process the generated sample as needed
+        console.log('Generated Sample:', result);
+    } catch (error) {
+        console.error('Error generating sample:', error);
+    }
 }
